9973cf61170fe350a5bb4170ff69a3559c1d64cc
Peter Grzybowski <peter@northern.tech>
2022-05-20 15:37:40 +0200

    Revert "rm tmp"
    This reverts commit 15dc9af46b68f76ef2f98d5d0141c1734c1e6156.

--- tests/utils/common/common.py
+++ tests/utils/common/common.py
@@ -412,7 +412,7 @@ def bp(index):
     sys.stdout.flush()
 
 def _run_bitbake(target, env_setup_cmd, capture=False):
-    cmd = "%s && rm -Rf tmp && bitbake %s" % (env_setup_cmd, target)
+    cmd = "%s && bitbake %s" % (env_setup_cmd, target)
     # ps = run_verbose("find \"%s\" -type d -and -name mender -and -exec git config --global --add safe.directory {} \\;" % env_setup_cmd, capture=subprocess.PIPE)
     # ps = run_verbose("wget -q https://go.dev/dl/go1.18.1.linux-amd64.tar.gz; mv /usr/local/go /usr/local/go-old; tar xzf go1.18.1.linux-amd64.tar.gz; mv go /usr/local/;", capture=subprocess.PIPE)
     # ps.wait()
@@ -454,7 +454,7 @@ def _run_bitbake(target, env_setup_cmd, capture=False):
             sys.stdout.flush()
             sys.stdout.write("\n}}}\n")
             sys.stdout.flush()
-# bp(1)
+            bp(1)
             raise e
 
     return output
